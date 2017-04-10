2d33ba90e158a4a1419247166db329c89fa6de4d
David Snider <dsnider.techie@gmail.com>
2017-04-10 17:48:51 -0500

    almost done?

--- MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/LoadCharacterFragment.java
+++ MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/LoadCharacterFragment.java
@@ -1,11 +1,58 @@
 package com.example.cis.mazeminotaurs;
 
+import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by My PC on 4/10/2017.
  */
 
 public class LoadCharacterFragment extends ListFragment {
+    private static final String TAG = "Family List Fragment";
 
+    private Family mFamily;
+
+    public LoadCharacterFragment(){
+        super();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        /*FamilyMember newMember = new Guardian("CL4P","TP");
+        mFamily.addFamilyMember(newMember);
+        FamilyMemberAdapter adapter = new FamilyMemberAdapter(mFamily.getFamily());
+        setListAdapter(adapter);*/
+    }
+
+    private class CharacterListAdapter extends ArrayAdapter<Character> {
+        public CharacterListAdapter(ArrayList<Character> list) {
+            super(getActivity(), 0, list);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = getActivity().getLayoutInflater()
+                        .inflate(R.layout.list_item_family_member, null);
+            }
+
+            Character character = getItem(position);
+
+            TextView nameTextView =
+                    (TextView)convertView
+                            .findViewById(R.id.family_member_list_item_nameTextView);
+            nameTextView.setText(character.getName());
+
+            return convertView;
+        }
+    }
 }
