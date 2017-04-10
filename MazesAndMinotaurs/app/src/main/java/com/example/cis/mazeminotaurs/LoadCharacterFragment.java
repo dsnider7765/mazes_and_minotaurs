9eea44ab2557492ae9fd1832c3682cd2d2d3f7b0
David Snider <dsnider.techie@gmail.com>
2017-04-10 17:30:12 -0500

    fresh start. created LoadCharacterFragment

---
+++ MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/LoadCharacterFragment.java
@@ -0,0 +1,11 @@
+package com.example.cis.mazeminotaurs;
+
+import android.support.v4.app.ListFragment;
+
+/**
+ * Created by My PC on 4/10/2017.
+ */
+
+public class LoadCharacterFragment extends ListFragment {
+
+}
